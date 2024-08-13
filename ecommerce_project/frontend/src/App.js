37491440267551d86180f1a8ebfd1a64acd68728
MinalAnwar <reachoutminalanwar@gmail.com>
2024-08-13 17:16:53 +0500

    tailwind css setup with react app done

--- ecommerce_project/frontend/src/App.js
+++ ecommerce_project/frontend/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      
+        <div className="p-6">
+          <h2 className="text-2xl font-bold text-gray-800">minal</h2>
+          <p className="mt-2 text-gray-600">girl</p>
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600">
+            Follow
+          </button>
+        </div>
     </div>
   );
 }
